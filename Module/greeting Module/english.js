88ebfccf383b7098657658dbe2982fdbcc7e8b48
santosh suryawanshi <santosh.suryawanshi28@gmail.com>
2017-04-09 21:15:01 +0530

    invoke greetings by using json file in module.

--- Module/greeting Module/english.js
+++ Module/greeting Module/english.js
@@ -1,4 +1,6 @@
+var greetings = require('./greetings.json');
 var greet = function(){
-  console.log("Hello");  
+ // console.log("Hello");  
+ console.log(greetings.en);
 }
 module.exports = greet;
--- Module/greeting Module/japanese.js
+++ Module/greeting Module/japanese.js
@@ -1,4 +1,6 @@
+var greetings = require('./greetings.json');
 var greet = function(){
-    console.log("konichiwa");
+   // console.log("konichiwa");
+   console.log(greetings.jp)
 }
 module.exports = greet;
