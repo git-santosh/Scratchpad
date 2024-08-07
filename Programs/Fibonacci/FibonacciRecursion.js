586bb06cc42a5630fae17004bae96aad0e99ea45
santosh <6646963+git-santosh@users.noreply.github.com>
2024-08-07 23:14:01 +0530

    fibonacci recursion

--- Programs/Fibonacci/FibonacciRecursion.js
+++ Programs/Fibonacci/FibonacciRecursion.js
@@ -16,3 +16,18 @@ function printFibonacci(number) {
 console.log(v1)
 console.log(v2)
 printFibonacci(10);
+
+// 2rd example
+//In programming, a recursive function is a function that calls itself, either directly or indirectly. This can be used to solve complex problems by breaking them down into smaller, simpler parts. For example, if you wanted to calculate the factorial of a number, you could use a recursive function to do so.
+function fibonacci(n) {
+  // Base case
+  if (n === 0 || n === 1) {
+    return n;
+  }
+  // Recursive case
+  else {
+    return fibonacci(n - 1) + fibonacci(n - 2);
+  }
+}
+
+fibonacci(12)
